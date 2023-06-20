c685f07cc218f310c43025c00bc87bea3577d889
AdrianaVlad <100144132+AdrianaVlad@users.noreply.github.com>
2023-06-21 01:29:37 +0300

    Add files via upload

--- Frontend/main/search.js
+++ Frontend/main/search.js
@@ -10,65 +10,73 @@ document.addEventListener('DOMContentLoaded', () => {
                 var tip = 'i';
                 var anotimp = 'a'
                 var regiune = 'r';
+                var colorGen = '';
+                var tipGen = '';
+                var anotimpGen = '';
+                var regiuneGen = '';
                 var cuvinte = txt.value.split(" ");
                 for (var i = 0; i < cuvinte.length; i++) {
                     var cuvant = cuvinte[i];
                     if (cuvant === "rosu" || cuvant === "rosie" || cuvant === "rosi" || cuvant === "rosii"
                         || cuvant === "red" || cuvant === "ros")
-                        color = "rosu";
+                        {color = "rosu"; colorGen="red";}
                     if (cuvant === "galben" || cuvant === "galbe" || cuvant === "glben"
                         || cuvant === "galbena" || cuvant === "galbene" || cuvant === "galbeni")
-                        color = "galben";
+                        {color = "galben"; colorGen="yellow";}
                     if (cuvant === "albastru" || cuvant === "albastra" || cuvant === "albastre"
                         || cuvant === "albastri" || cuvant === "alabastri" || cuvant === "albastr")
-                        color = "albastru";
+                        {color = "albastru"; colorGen="blue"}
                     if (cuvant === "portocaliu" || cuvant === "portocalie" || cuvant === "portocala"
                         || cuvant === "prtocal" || cuvant === "orange" || cuvant === "portocalii")
-                        color = "portocaliu";
+                        {color = "portocaliu"; colorGen="orange";}
                     if (cuvant === "verde" || cuvant === "verzi" || cuvant === "verd"
                         || cuvant === "verzi" || cuvant === "verzui" || cuvant === "turcuaz")
-                        color = "verde";
+                        {color = "verde"; colorGen="green"}
                     if (cuvant === "alba" || cuvant === "albe" || cuvant === "alb"
                         || cuvant === "albi" || cuvant === "ab" || cuvant === "al")
-                        color = "alba";
+                        {color = "alba"; colorGen="white"}
                     if (cuvant === "roz" || cuvant === "roze" || cuvant === "rozi")
-                        color = "roz";
+                        {color = "roz"; colorGen="pink";}
                     if (cuvant === "lila" || cuvant === "lil" || cuvant === "violet"
                         || cuvant === "mov" || cuvant === "move" || cuvant === "movi")
-                        color = "lila";
+                        {color = "lila"; colorGen="purple";}
                     if (cuvant === "medicinala" || cuvant === "medicinale" || cuvant === "medicinal")
-                        tip = "medicinala";
+                        {tip = "medicinala"; tipGen="medicinal"}
                     if (cuvant === "feriga" || cuvant === "ferigi" || cuvant === "ferige")
-                        tip = "feriga";
+                        {tip = "feriga"; tipGen="fig";}
                     if (cuvant === "carnivor" || cuvant === "carnivora" || cuvant === "carnivore" || cuvant === "carnivori")
-                        tip = "carnivor";
+                        {tip = "carnivor"; tipGen="carnivorous";}
                     if (cuvant === "suculent" || cuvant === "suculenta" || cuvant === "suculente" || cuvant === "suculenti")
-                        tip = "suculent";
+                        {tip = "suculent"; tipGen="succulent";}
                     if (cuvant === "aromatice" || cuvant === "aromatic" || cuvant === "aromatici" || cuvant === "aroma")
-                        tip = "aromatice";
+                        {tip = "aromatice"; tipGen="aromatic";}
                     if (cuvant === "montana" || cuvant === "munte" || cuvant === "munti" || cuvant === "inalte")
-                        regiune = "montana";
+                        {regiune = "montana"; regiuneGen="mountain";}
                     if (cuvant === "ecuatoriala" || cuvant === "ecuator" || cuvant === "ecuatorial")
-                        regiune = "ecuatoriala";
+                        {regiune = "ecuatoriala"; regiuneGen="equator";}
                     if (cuvant === "mlastina" || cuvant === "mlasinoasa" || cuvant === "mlastinos" || cuvant === "mlastinose")
-                        regiune = "mlastina";
+                        {regiune = "mlastina"; regiuneGen="swamp";}
                     if (cuvant === "deset" || cuvant === "desert" || cuvant === "desertica" || cuvant === "desertice")
-                        regiune = "desertica";
+                        {regiune = "desertica"; regiuneGen="desert";}
                     if (cuvant === "jungla" || cuvant === "salbatica" || cuvant === "jungl")
-                        regiune = "jungla";
+                        {regiune = "jungla"; regiuneGen="jungle";}
                     if (cuvant === "primavara" || cuvant === "primavaratice" || cuvant === "primavaratic")
-                        anotimp = "primavara";
+                        {anotimp = "primavara"; anotimpGen="spring";}
                     if (cuvant === "vara" || cuvant === "varatice" || cuvant === "varatic")
-                        anotimp = "vara";
+                        {anotimp = "vara"; anotimpGen="summer";}
                     if (cuvant === "toamna" || cuvant === "tomnatice" || cuvant === "tomatictic")
-                        anotimp = "toamna";
+                        {anotimp = "toamna"; anotimpGen="autumn";}
                     if (cuvant === "iarna" || cuvant === "iernatice" || cuvant === "iernatic")
-                        anotimp = "iarna";
+                        {anotimp = "iarna"; anotimpGen="winter";}
                 }
                 localStorage.setItem("color",color);
                 localStorage.setItem("tip", tip);
                 localStorage.setItem("anotimp", anotimp);
                 localStorage.setItem("regiune", regiune);
+                localStorage.setItem("colorGen",colorGen);
+                localStorage.setItem("tipGen", tipGen);
+                localStorage.setItem("anotimpGen", anotimpGen);
+                localStorage.setItem("regiuneGen", regiuneGen);
                 window.location.href = "../search/search.html";
             }
             )
